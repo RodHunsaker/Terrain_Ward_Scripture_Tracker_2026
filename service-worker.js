6dde4a4fc8d2deed632f228d69a2c5f37458b173
RodHunsaker <108632488+RodHunsaker@users.noreply.github.com>
2025-12-10 14:34:22 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,44 @@
+const CACHE_NAME = "iron-rod-tracker-v1";
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable.png"
+];
+
+// Install and cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate and remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercept network requests
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
